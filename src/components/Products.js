addf37e84dd4daa061f7f06bd004b52195f5892a
emlahieu98 <hieucr98tb@gmail.com>
2020-12-29 17:43:49 +0700

    fix create

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,31 +1,34 @@
 import React, { Component } from "react";
+import Fade from "react-reveal/Fade"
 //import formatCurrency from "../util";
 
 export default class Products extends Component {
   render() {
     return (
       <div>
-        <ul className="products">
-          {this.props.products.map((product) => (
-            <li key={product._id}>
-              <div className="product">
-                <a href={"#" + product._id}>
-                  <img src={product.image} alt={product.title}></img>
-                  <p>{product.title}</p>
-                </a>
-                <div className="product-price">
-                  <div>{product.price}$</div>
-                  <button
-                    onClick={() => this.props.addToCart(product)}
-                    className="button primary"
-                  >
-                    Add To Cart
-                  </button>
+        <Fade bottom cascade>
+          <ul className="products">
+            {this.props.products.map((product) => (
+              <li key={product._id}>
+                <div className="product">
+                  <a href={"#" + product._id}>
+                    <img src={product.image} alt={product.title}></img>
+                    <p>{product.title}</p>
+                  </a>
+                  <div className="product-price">
+                    <div>{product.price}$</div>
+                    <button
+                      onClick={() => this.props.addToCart(product)}
+                      className="button primary"
+                    >
+                      Add To Cart
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+              </li>
+            ))}
+          </ul>
+        </Fade>
       </div>
     );
   }
